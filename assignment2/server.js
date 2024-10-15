c2a29cd4834b5884520d0a2f2909b89e1e7c6081
DongN2 <dngo13@myseneca.ca>
2024-10-14 23:07:31 -0400

    feat: Implement assignment 2 follow the module with function get[Dngo13]

--- assignment2/server.js
+++ assignment2/server.js
@@ -1,6 +1,53 @@
-app.get('/api', (req, res) => {
-    res.json({
-        message: 'Hello world',
-        data: 'This is my first API'
+const storeService = require('./store-service.js');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Serve static files from the "public" folder
+app.use(express.static('public'));
+
+// Redirect root to "/about"
+app.get('/', (req, res) => {
+    res.redirect('/about');
+});
+
+// Serve the about page
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+app.get('/shop', (req, res) => {
+    storeService.getPublishedItems()
+        .then(data => res.json(data))
+        .catch(err => res.status(500).json({ message: err }));
+});
+
+
+app.get('/items', (req, res) => {
+    storeService.getAllItems()
+        .then(data => res.json(data))
+        .catch(err => res.status(500).json({ message: err }));
+});
+
+
+app.get('/categories', (req, res) => {
+    storeService.getCategories()
+        .then(data => res.json(data))
+        .catch(err => res.status(500).json({ message: err }));
+});
+
+
+app.use((req, res) => {
+    res.status(404).send('Page Not Found');
+});
+
+storeService.initialize()
+    .then(() => {
+        app.listen(8080, () => {
+            console.log('Server started on http://localhost:8080');
+        });
     })
-})
+    .catch((err) => {
+        console.error(`Failed to start server: ${err}`);
+    });
