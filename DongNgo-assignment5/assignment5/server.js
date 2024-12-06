009ddcb4533837ce5e574b880a6b9d139cd24cd9
DongN2 <dngo13@myseneca.ca>
2024-12-05 23:53:58 -0500

    feat: Implement vercel and setting for assignment 5[Dngo13]

--- DongNgo-assignment5/assignment5/server.js
+++ DongNgo-assignment5/assignment5/server.js
@@ -6,7 +6,7 @@
 * 
 *  Name: Dong/Lynne Ngo Student ID: 129432233 Date: 2024-12-05
 *
-*  Vercel Web App URL: ________________________________________________________
+*  Vercel Web App URL: https://web-322-yf8u.vercel.app/about
 *
 *  GitHub Repository URL: https://github.com/DongNgoo/WEB322.git
 *
